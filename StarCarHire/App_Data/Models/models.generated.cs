7b673206b33d227882a11adedcdaae1ec1f864bf
Paul McCafferty <paul.mccafferty@izon.live>
2021-07-13 16:54:56 +0100

    Results page started

--- StarCarHire/App_Data/Models/models.generated.cs
+++ StarCarHire/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "892f25a165a9caa3")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
 
 namespace Umbraco.Web.PublishedModels
 {
