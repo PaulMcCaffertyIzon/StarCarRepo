72bd21ab074f1c4f5223c9bb3e5ffc7e668787f0
Paul McCafferty <paul.mccafferty@izon.live>
2021-07-16 11:27:53 +0100

    Vehicle Model configured

---
+++ StarCarHire/Models/VehicleModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StarCarHire.Models
+{
+    public class VehicleModel
+    {
+        public string VehicleMake { get; set; }
+
+        public string Vehicle { get; set; }
+
+        public int VehicleYear { get; set; }
+
+        public string VehicleOrigin { get; set; }
+
+        public string VehicleExteriorColour { get; set; }
+
+        public string VehicleInteriorColour { get; set; }
+
+        public int VehiclePassengers { get; set; }
+
+        public bool VehicleConvertible { get; set; }
+
+        public bool VehicleDriveSide { get; set; }
+
+        public string VehicleLocation { get; set; }
+
+        public string VehicleCondition { get; set; }
+
+        public string VehicleAdditionalInfo { get; set; }
+
+        public string VehicleAvailableFor { get; set; }
+
+        public decimal VehicleHalfDayPrice { get; set; }
+
+        public decimal VehicleFullDayPrice { get; set; }
+
+        public decimal VehicleHourlyPrice { get; set; }
+
+
+    }
+}
